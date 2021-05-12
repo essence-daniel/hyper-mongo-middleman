b7e091ca523b0d02eb094c720f52003dd7aa68cb
Imag <44352064+Imag@users.noreply.github.com>
2021-05-11 19:18:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const helmet = require("helmet");
 const app = express();
+const mongoose = require("mongoose");
+const MONGO_DB_URI = process.env.MONGO_DB_URI || "";
+
+var database = mongoose
+  .connect(MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => console.log("Conected to MongoDB"))
+  .catch((err) => console.log("Failed to connect to MongoDB"));
+
 
 require("dotenv").config();
 
